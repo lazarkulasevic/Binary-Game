695cfd9ca6dfa414c467388d82d08c4f3c0cb924
lazarkulasevic <lazar.kulasevic@gmail.com>
2020-09-12 00:11:02 +0200

    working on demo btn

---
+++ script.js
@@ -0,0 +1,177 @@
+let tableTable = document.getElementById('table');
+let tableResults = document.querySelector('.results');
+let spanPercent = document.querySelector('span');
+let faBtn = document.querySelector('.fa');
+
+let mainNumber = Math.floor(Math.random() * 100) + 1;
+let counter = 1;
+
+let userAccuracy = {
+    accuracy: 100,
+    win: 0,
+    numberOfPlay: 0
+}
+
+for (let i = 1; i <= 10; i++) {
+    let trElement = document.createElement('tr');
+    tableTable.appendChild(trElement);
+
+    for (let i = 1; i <= 10; i++) {
+        let tdElement = document.createElement('td');
+        tdElement.classList.add('number')
+        trElement.appendChild(tdElement);
+    }
+}
+
+let tdElementAll = document.querySelectorAll('.number');
+
+tdElementAll.forEach((td, i) => {
+    let tdNumber = document.createTextNode(`${i + 1}`);
+    td.appendChild(tdNumber);
+});
+
+tableTable.addEventListener('click', event => {
+    if (event.target.tagName == 'TD') {
+        if (event.target.classList.contains('clicked')) return;
+        event.target.classList.add('clicked');
+
+        if (event.target.textContent == mainNumber) {
+            playersAccuracy();
+            tableResultsPrint(counter);
+            congrats();
+        } else {
+            tableResultsPrint(counter);
+            counter++;
+            if (counter > 7) {
+                playersAccuracy();
+                alertPlayer();
+            }
+        }
+    }
+});
+
+console.log('trazeni', mainNumber);
+
+
+
+for (let i = 1; i <= 8; i++) {
+    let trElement = document.createElement('tr');
+    trElement.classList.add('tr-results')
+    tableResults.appendChild(trElement);
+}
+
+let trElementAllRes = document.querySelectorAll('.tr-results');
+
+for (let i = 1; i <= 3; i++) {
+    let tdElement = document.createElement('td');
+    trElementAllRes[0].appendChild(tdElement);
+}
+
+let tdText1 = document.createTextNode('Number of attempts');
+let tdText2 = document.createTextNode('The number is');
+let tdText3 = document.createTextNode('Attempts left');
+
+let tdElementAllRes = document.querySelectorAll('.tr-results td');
+
+tdElementAllRes[0].appendChild(tdText1);
+tdElementAllRes[1].appendChild(tdText2);
+tdElementAllRes[2].appendChild(tdText3);
+
+function tableResultsPrint(counter) {
+    let trElementAllRes = document.querySelectorAll('.tr-results');
+
+    for (let i = 1; i <= 3; i++) {
+        let tdElement = document.createElement('td');
+        tdElement.classList.add(`num${counter}`)
+        trElementAllRes[counter].appendChild(tdElement);
+    }
+
+    let classCounter = 'num' + counter;
+    let tdElementAllRes = document.querySelectorAll(`.${classCounter}`)
+    let greater = 'Greater than', less = 'Less than';
+    let greaterLess = event.target.textContent > mainNumber ? less : greater;
+
+    let numOfTry = document.createTextNode(`${counter}`);
+    let greaterOrLess = document.createTextNode(`${greaterLess} ${event.target.textContent}`);
+    let triesLeft = document.createTextNode(`${7 - counter}`);
+
+    tdElementAllRes[0].appendChild(numOfTry);
+    tdElementAllRes[1].appendChild(greaterOrLess);
+    tdElementAllRes[2].appendChild(triesLeft);
+}
+
+function alertPlayer() {
+    alert(`Game over! The number we were looking for is ${mainNumber}`);
+    location.reload();
+}
+
+function congrats() {
+    alert(`Congratulations! The number is ${mainNumber}`);
+    location.reload();
+}
+
+// LOCAL STORAGE
+if (JSON.parse(localStorage.getItem('userAccuracy')) === null) {
+    localStorage.setItem('userAccuracy', JSON.stringify(userAccuracy));
+} else {
+    userAccuracy = JSON.parse(localStorage.getItem('userAccuracy'));
+    spanPercent.textContent = `${userAccuracy.accuracy}%`;
+}
+
+function playersAccuracy() {
+    if (counter <= 7) {
+        userAccuracy.win += 1;
+    }
+    userAccuracy.numberOfPlay += 1;
+    userAccuracy.accuracy = Math.round(userAccuracy.win / userAccuracy.numberOfPlay * 100);
+    spanPercent.textContent = `${userAccuracy.accuracy}%`;
+
+    return localStorage.setItem('userAccuracy', JSON.stringify(userAccuracy));
+}
+
+function binarySearch() {
+    let target = 50;
+    let bot = 0, top = 100;
+    let fields = [];
+
+    while (target != mainNumber) {
+        fields.push(target);
+
+        if (target < mainNumber) {
+            bot = target;
+        } else {
+            top = target;
+        }
+        target = Math.round((top + bot) / 2);
+    }
+    console.log('poslednji broj je', target);
+    fields.push(target);
+
+    return fields;
+}
+
+function demo() {
+    let i = 0;
+    let coloring = setInterval(() => {
+        let num = fields[i];
+        i += 1;
+
+        console.log(num);
+
+        tdElementAll[num - 1].classList.add('clicked');
+        if (i == fields.length) {
+            tdElementAll[num-1].classList.add('wanted');
+            clearInterval(coloring);
+        }
+
+        tableResultsPrint(i);
+
+    }, 800);
+}
+
+faBtn.addEventListener('click', event => {
+    fields = binarySearch();
+    demo();
+    
+
+}, {once: true});
